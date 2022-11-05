368dfc709ae9597a2ebb6d74fbe53ee4145b2396
Javier García-Arango <javigarc@iMac-de-Javier.local>
2022-11-05 20:30:22 +0100

    gnl2

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "libft.h"
 
 char	*ft_addbuffer(char *ldr, char *buff)
 {
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "libft.h"
 
 char	*ft_createline(char *src, int len)
 {
